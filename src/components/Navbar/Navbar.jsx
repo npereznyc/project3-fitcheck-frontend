fac7c60ab29437f10b04081b01989fe42ad91cd2
mary3210 <maryrodri3210@gmail.com>
2023-01-04 09:34:03 -0600

    updated feed.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import {Link} from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const Navbar = (props) => {
     return (
         <nav className="nav">
+            
             <Link to='/'>
+            <FontAwesomeIcon icon="fa-solid fa-house" />
                 <h1>Home</h1>
             </Link>
             <Link to='/createpost'>
