099e76958785daf856fbface27ca8e3a6a481099
jordbort <jordan.bortner@gmail.com>
2023-01-07 17:36:40 -0500

    Added authentication functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,32 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHouse, faCirclePlus, faUser } from '@fortawesome/free-solid-svg-icons'
+import { UserContext } from '../../data'
+import { useState } from 'react'
 import './App.css'
 import Header from '../Header/Header'
 import Navbar from '../Navbar/Navbar'
 import Feed from '../Feed/Feed'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHouse, faCirclePlus, faUser } from '@fortawesome/free-solid-svg-icons'
-// import CreatePost from '../../pages/CreatePost'
-// import Update from '../../pages/EditPost'
 
 function App() {
     library.add(faHouse, faCirclePlus, faUser)
+
+    const { Provider : UserInfo } = UserContext
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [currentUser, setCurrentUser] = useState(null)
+
     return (
         <div className="App">
-            <Header />
-            <Navbar />
-            <Feed />
+            <UserInfo value={{
+                isAuthenticated,
+                currentUser,
+                setAuth: setIsAuthenticated,
+                setUser: setCurrentUser
+            }}>
+                <Header />
+                <Navbar />
+                <Feed />
+            </UserInfo>
         </div>
     )
 }
